be5a94e37a59976e4e1f400f6e360a2aee15a5fe
Evgeniy <106624002+EvgeniyRyabov@users.noreply.github.com>
2022-06-01 23:42:37 +0300

    Add files via upload

---
+++ HomeworkLesson1_1.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //возможно, так не совсем разумно писать, но пока что имеею:)
+        System.out.println("H");
+        System.out.println("e");
+        System.out.println("l");
+        System.out.println("l");
+        System.out.println("o");
+        System.out.println(" ");
+        System.out.println("w");
+        System.out.println("o");
+        System.out.println("r");
+        System.out.println("l");
+        System.out.println("d");
+        System.out.println("!");
+    }
+}
