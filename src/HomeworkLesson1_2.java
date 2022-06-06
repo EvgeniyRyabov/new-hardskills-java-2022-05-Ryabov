334e3d4c20dfb2856dcd33b2a4b7bb89f961614e
EvgeniyRyabov <Freeman2022>
2022-06-06 10:43:05 +0300

    Home work apdated

--- src/HomeworkLesson1_2.java
+++ src/HomeworkLesson1_2.java
@@ -1,6 +1,6 @@
 public class HomeworkLesson1_2 {
     public static void main(String[] args) {
-        for (byte i = 0; i < 11; i ++)
+        for (byte i = 0; i < 12; i ++)
         System.out.print(i + " ");
     }
 }
