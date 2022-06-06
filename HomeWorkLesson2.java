c1d9864cdd14a333e4247093dc310a40b25bd6af
EvgeniyRyabov <Freeman2022>
2022-06-06 11:29:31 +0300

    Add homework for lesson 2

---
+++ HomeWorkLesson2.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int salary = 1_850;
+        double taxes = 0.13 + 0.01;
+        System.out.println(salary - salary * taxes + " бел. руб.");
+    }
+}
