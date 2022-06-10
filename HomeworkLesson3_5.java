05c0d863ef115865c3ce8489cc67271c038957f8
EvgeniyRyabov <Freeman2022>
2022-06-10 17:57:35 +0300

    Add honework for lesson 3

---
+++ HomeworkLesson3_5.java
@@ -0,0 +1,9 @@
+public class HomeworkLesson3_5 {
+    public static void main(String[] args) {
+        String s = "";
+        for (int i = 1; i <= 10; i++){
+            System.out.println(s += "*");
+
+        }
+    }
+}
